47635aae1790cec2ac5c19ed4a84f12404c965f5
ab1kenobi <107165784+ab1kenobi@users.noreply.github.com>
2025-02-07 23:51:41 -0600

    Added more ig

--- flu_bot.py
+++ flu_bot.py
@@ -1,4 +1,6 @@
 import pandas as py
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score
+
 
