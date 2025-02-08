f8c043f4942f1bffaa95d248521955b4fa644768
ab1kenobi <107165784+ab1kenobi@users.noreply.github.com>
2025-02-08 00:16:32 -0600

    added stuff

--- flu_bot.py
+++ flu_bot.py
@@ -12,4 +12,16 @@ print(data2.head())
 merged_data = py.merge(data1, data2)
 print(merged_data.head())
 
+#we are using a RandomForestClassifier model which means that we will be building a tree
+#this model builds a tree and based on the roots of the tree, it will make predictions
+#in order to do this we first need to extract relevant features
+#this model will be built on the avg.temp, the lat, the lon
+#based on all of these features that we train it on, it should determine the likelihood of an outbreak
+#step 1 is features
+merged_data['latitude'] = merged_data['lat']
+merged_data['longitude'] = merged_data['lon']
+merged_data['avg_temp'] = merged_data['avg.temp']
+
+
+
 
