82e035b3f70cf305a46cca617bde9fe8173fc62e
ab1kenobi <107165784+ab1kenobi@users.noreply.github.com>
2025-02-08 00:07:38 -0600

    Merged files to add temp to common file can build model now need to clean file

--- flu_bot.py
+++ flu_bot.py
@@ -3,4 +3,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score
 
+data1 = py.read_csv('avg_temp.csv')
+data2 = py.read_csv('h5n1_cdc.csv')
+
+print(data1.head())
+print(data2.head())
+
+merged_data = py.merge(data1, data2)
+print(merged_data.head())
+
 
